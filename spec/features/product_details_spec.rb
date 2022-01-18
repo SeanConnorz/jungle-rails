519684b3103e458250146240526481b2dbe19450
SeanConnorz <seanandhughes@gmail.com>
2022-01-18 11:41:05 -0500

    Refactoring, getting rid of inspect and puts

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  "Testing",
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    click_link('Testing')
+    sleep(1)
+    # save_screenshot
+    expect(page).to have_content 'Apparel'
+  end
+
+end
